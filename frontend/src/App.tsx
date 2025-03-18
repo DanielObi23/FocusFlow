80e8bd190d85788396b05b3d4470b8721296141d
Chibuikem <chibuikemonwuobi23@gmail.com>
2025-03-18 21:57:33 +0000

    added darkmode and animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import Header from "./components/Header"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Header from "./components/Header"
 import DashboardPage from "./pages/DashboardPage"
 import LearningPathsPage from "./pages/LearningPathsPage"
 import NotFoundPage from "./pages/NotFoundPage"
 import ProfilePage from "./pages/ProfilePage"
 import SkillsPage from "./pages/SkillsPage"
+import ContactSupportPage from "./pages/ContactSupportPage"
 
 export default function App() {
   return (
@@ -16,8 +17,11 @@ export default function App() {
         <Route path="/paths" element={<LearningPathsPage />} />
         <Route path="/skills" element={<SkillsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/support" element={<ContactSupportPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   )
 }
+
+
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,26 +6,26 @@ export default function Header() {
   return (
     <header 
       style={{ fontFamily: "Inter" }} 
-      className="h-30 w-full flex flex-col md:flex-row p-2 items-center justify-between px-6 bg-sky-100"
+      className="h-35 w-full flex flex-col md:flex-row p-2 items-center justify-between px-6 bg-sky-100 dark:bg-gray-900 dark:text-white"
     >
       <div className="flex items-center gap-2">
         <Link to="/profile">
-          <img src={avatar} alt="profile settings" className="size-15 md:size-17 lg:hidden rounded-full hover:animate-bounce"/>
+          <img src={avatar} alt="profile settings" className="size-15 md:size-17 lg:hidden rounded-full"/>
         </Link>
         <Link to="/">
-          <img src={logo} alt="FocusFlow logo" className="size-18 hidden lg:block"/>
+          <img src={logo} alt="FocusFlow logo" className="size-18 hidden lg:block xl:active:size-24 xl:active:animate-bounce xl:active:translate-x-150 duration-3000"/>
         </Link>
         <h1 className="text-4xl md:text-5xl mb-2 font-bold self-end">FocusFlow</h1>
       </div>
       
       <nav className="flex items-center gap-15 font-semibold text-lg mt-5 md:text-xl md:me-4">
         <div className="flex gap-10 lg:gap-6">
-          <Link to="/dashboard" className="hover:text-sky-700 hover:border-b-2 border-black hover:mx-3 duration-350 ease-in">Dashboard</Link>
-          <Link to="/paths" className="hover:text-sky-700 hover:border-b-2 border-black hover:mx-3 duration-350 ease-in"><span className="hidden xl:inline">Learning</span> Paths</Link>
-          <Link to="/skills" className="hover:text-sky-700 hover:border-b-2 border-black hover:mx-3 duration-350 ease-in"><span className="hidden xl:inline">My</span> Skills</Link>
+          <Link to="/dashboard" className="focus:outline-2 focus:border-b-0 focus:outline-offset-7 hover:text-sky-700 dark:hover:text-sky-300 hover:border-b-2 border-black dark:border-white hover:mx-3 duration-350 ease-in">Dashboard</Link>
+          <Link to="/paths" className="focus:outline-2 focus:border-b-0 focus:outline-offset-7 hover:text-sky-700 dark:hover:text-sky-300 hover:border-b-2 border-black dark:border-white hover:mx-3 duration-350 ease-in"><span className="hidden xl:inline">Learning</span> Paths</Link>
+          <Link to="/skills" className="focus:outline-2 focus:border-b-0 focus:outline-offset-7 hover:text-sky-700 dark:hover:text-sky-300 hover:border-b-2 border-black dark:border-white hover:mx-3 duration-350 ease-in"><span className="hidden xl:inline">My</span> Skills</Link>
         </div>
         <Link to="/profile" className="lg:ms-20 lg:me-10 hidden lg:block">
-          <img src={avatar} alt="profile settings" className="size-15 lg:size-15 rounded-full hover:animate-bounce"/>
+          <img src={avatar} alt="profile settings" className="size-15 lg:size-15 rounded-full"/>
         </Link>
       </nav>
     </header>
